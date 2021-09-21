3109f6fef06b7d9d77c05b17b4ce5818dc516ad0
munnanoori <munnanoori.venkat@gmail.com>
2021-09-21 20:45:18 +0530

    Adding scheduled delay

--- rabbitmqconsumer/src/main/java/rabbitmq/consumer/RabbitMQConsumer.java
+++ rabbitmqconsumer/src/main/java/rabbitmq/consumer/RabbitMQConsumer.java
@@ -1,6 +1,7 @@
 package rabbitmq.consumer;
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import rabbitmq.consumer.entity.Order;
@@ -10,6 +11,6 @@ public class RabbitMQConsumer {
 
 	@RabbitListener(queues = "${queue.name}")
 	public void consumeMessage(Order order) {
-		System.out.println("Received Order ID: " + order.getOrderID() + "on date: " + order.getDate());
+		System.out.println("Received Order ID: " + order.getOrderID() + " on date: " + order.getDate());
 	}
 }
--- rabbitmqconsumer/src/main/java/rabbitmq/consumer/RabbitmqconsumerApplication.java
+++ rabbitmqconsumer/src/main/java/rabbitmq/consumer/RabbitmqconsumerApplication.java
@@ -3,19 +3,26 @@ package rabbitmq.consumer;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
 
 @SpringBootApplication
+@EnableScheduling
 public class RabbitmqconsumerApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(RabbitmqconsumerApplication.class, args);
 	}
 
+	@Scheduled(fixedDelay = 2000)
+	public void executeTask() {
+		
+	}
+	
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("I am consumer");
-		while(true);
 		
 	}
 
